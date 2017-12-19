b5fcc5724bbb8a42e84261f75a15e0a35a87f80c
cottom <jerry_ii@outlook.com>
2017-12-19 18:13:06 +0800

    fix: token

--- public/js/blog-item.js
+++ public/js/blog-item.js
@@ -5,8 +5,8 @@ function initComment () {
   const id = dom.dataset.id
   new GhTalk({  // eslint-disable-line
     selector: '.blog-comment',
-    clientId: 'e77789a76f3d4177222a',
-    clientSecret: '71336d3afe48d3136030e683b968df6f6a9bb943',
+    clientId: 'acdf02d798960d98dc7d',
+    clientSecret: '647f54566aef4e03ebbc80309eb8adb501fb7d63',
     owner: 'jerry-i',
     repo: 'blog',
     issueId: id
